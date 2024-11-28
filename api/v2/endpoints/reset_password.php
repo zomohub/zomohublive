279ce99d56fe931a2d05097a9cbf1279cf4f2ddf
Fahad <fahad@dpdweb.com>
2024-11-29 01:14:13 +0500

    testing

--- api/v2/endpoints/reset_password.php
+++ api/v2/endpoints/reset_password.php
@@ -2,7 +2,7 @@
 
 if (!empty($_POST['new_password']) &&  !empty($_POST['code'])) {
 	
-    //$user_id  = explode("_", $_POST['code']);
+    $user_id  = explode("_", $_POST['code']);
 
 	$update = true;
 
